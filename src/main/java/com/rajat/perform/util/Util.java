0543a42cfd00aba9781b22ef4d0730435ab75532
RAJAT MANDEWALA <rajatmandewala@gmail.com>
2020-02-11 08:04:15 +0530

    udate util date

--- src/main/java/com/rajat/perform/util/Util.java
+++ src/main/java/com/rajat/perform/util/Util.java
@@ -1,6 +1,8 @@
 package com.rajat.perform.util;
 
 import org.springframework.stereotype.Component;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class Util {
@@ -8,7 +10,10 @@ public class Util {
 	
 	
 	public String generateInsertQuery(String str) {
-		
+		String startDateString = "08-12-2017";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		System.out.println(LocalDate.parse(startDateString, formatter).format(formatter2));
 		
 		
 		
